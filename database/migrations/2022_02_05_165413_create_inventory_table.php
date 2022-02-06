6c6fb31fa489d2af626387aa9e3ae2a8a6e55c53
olakehinde <olakehinde59@gmail.com>
2022-02-06 04:41:30 +0100

    changed table name to inventories in migration file

--- database/migrations/2022_02_05_165413_create_inventory_table.php
+++ database/migrations/2022_02_05_165413_create_inventory_table.php
@@ -13,7 +13,7 @@ class CreateInventoryTable extends Migration
      */
     public function up()
     {
-        Schema::create('inventory', function (Blueprint $table) {
+        Schema::create('inventories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->float('price', 8, 2);
